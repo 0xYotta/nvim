b314167a1de0cc2bfacf454c2caad11172b2317a
0xYotta <ctf0000000@gmail.com>
2025-01-26 10:32:16 +0500

    remove ts-tools, add vtsls and basic ts configuration via lazy extras

--- lua/plugins/coding/typescript.lua
+++ lua/plugins/coding/typescript.lua
@@ -1,4 +1,9 @@
 return {
+
+  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
+  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+
   -- NOTE: Autoclosing tags
   {
     "windwp/nvim-ts-autotag",
@@ -19,36 +24,40 @@ return {
     -- this is equivalent to setup({}) function
   },
 
-  --NOTE: Typescript
-  {
-    "pmizio/typescript-tools.nvim",
-    lazy = false,
-    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-    opts = function()
-      require("typescript-tools").setup({
-        settings = {
-          jsx_close_tag = {
-            enable = true,
-            filetypes = { "javascriptreact", "typescriptreact" },
-          },
-          tsserver_file_preferences = {
-            quotePreference = "single",
-            includeInlayParameterNameHints = "all",
-            includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-
-            includeInlayVariableTypeHints = true,
-            includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-
-            includeInlayFunctionLikeReturnTypeHints = true,
-          },
-        },
-        on_attach = function(_, bufnr)
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            buffer = bufnr,
-            command = "EslintFixAll",
-          })
-        end,
-      })
-    end,
-  },
+  --
+  -- --NOTE: Typescript tools
+  -- {
+  --   "pmizio/typescript-tools.nvim",
+  --   lazy = false,
+  --   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  --   opts = {
+  --     settings = {
+  --       tsserver = {
+  --         inlayHints = {
+  --           enable = true,
+  --         },
+  --       },
+  --       jsx_close_tag = {
+  --         enable = true,
+  --         filetypes = { "javascriptreact", "typescriptreact" },
+  --       },
+  --       tsserver_file_preferences = {
+  --         quotePreference = "single",
+  --         includeInlayParameterNameHints = "all",
+  --         -- includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --
+  --         -- includeInlayVariableTypeHints = true,
+  --         -- includeInlayVariableTypeHintsWhenTypeMatchesName = true,
+  --
+  --         includeInlayFunctionLikeReturnTypeHints = true,
+  --       },
+  --     },
+  --     on_attach = function(_, bufnr)
+  --       vim.api.nvim_create_autocmd("BufWritePre", {
+  --         buffer = bufnr,
+  --         command = "EslintFixAll",
+  --       })
+  --     end,
+  --   },
+  -- },
 }
