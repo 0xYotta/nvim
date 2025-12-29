158df4fa2d9184034447c7ea974189a439797263
0xYotta <ctf0000000@gmail.com>
2025-12-29 14:56:58 +0700

    zig minor fixes

--- lua/plugins/coding/zig.lua
+++ lua/plugins/coding/zig.lua
@@ -4,6 +4,14 @@ return {
     opts = {
       servers = {
         zls = {
+
+          filetypes = { "zig", "zon" },
+          settings = {
+            zls = {
+              enable_snippets = true, -- Or false, depending on preference
+              -- Other zls specific settings
+            },
+          },
           cmd = { "${HOME}/.zvm/bin/zls" },
         },
       },
